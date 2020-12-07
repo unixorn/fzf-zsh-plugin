de8b09a1b9726e5d4c1f92b8d3be36ff62776b06
Jonathan Barratt <jonathan.barratt@gmail.com>
2020-12-07 16:27:19 +0700

    fix typo in FZF_DEFAULT_OPTS env var
    fzf looks for FZF_DEFAULT_OPTS  not ,,,_OPS

--- fzf-zsh-plugin.plugin.zsh
+++ fzf-zsh-plugin.plugin.zsh
@@ -28,7 +28,7 @@ fi
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
-export FZF_DEFAULT_OPS='--extended'
+export FZF_DEFAULT_OPTS='--extended'
 export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 
 # If fd command is installed, use it instead of find
