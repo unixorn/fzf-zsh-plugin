50bd50a5f0017a9ce2d2536f8a845cfedeb280b0
Joe Block <jpb@unixorn.net>
2021-05-05 22:58:31 -0600

    Add an fzf wrapper for z
    Like the normal z when used with arguments but displays an fzf prompt when used without them.

--- fzf-zsh-plugin.plugin.zsh
+++ fzf-zsh-plugin.plugin.zsh
@@ -77,3 +77,12 @@ alias fkill='fzf-kill'
 if [[ -d ~/.fzf/man ]]; then
   export MANPATH="$MANPATH:~/.fzf/man"
 fi
+
+if has z; then
+  unalias z 2> /dev/null
+  # like normal z when used with arguments but displays an fzf prompt when used without.
+  z() {
+    [ $# -gt 0 ] && _z "$*" && return
+    cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
+  }
+fi
