3f24585f830162030ea56c754118628e69fa367e
Joe Block <jpb@unixorn.net>
2021-05-27 19:14:58 -0600

    Fix z function handling

--- fzf-zsh-plugin.plugin.zsh
+++ fzf-zsh-plugin.plugin.zsh
@@ -88,7 +88,7 @@ fi
 if has z; then
   unalias z 2> /dev/null
   # like normal z when used with arguments but displays an fzf prompt when used without.
-  z() {
+  function z() {
     [ $# -gt 0 ] && _z "$*" && return
     cd "$(_z -l 2>&1 | fzf --height 40% --nth 2.. --reverse --inline-info +s --tac --query "${*##-* }" | sed 's/^[0-9,.]* *//')"
   }
