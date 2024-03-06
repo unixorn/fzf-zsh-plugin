b5d738a380d8ccee47abe1bf53da4d35886b88bf
Александр Крамарев <pochemuto@gmail.com>
2024-03-06 21:03:08 +0100

    Fix "fzf-zsh-plugin.plugin.zsh…(anon):50: parse error: condition expected: _fzf_has"
    Signed-off-by: Alexander Kramarev <pochemuto@yandex-team.ru>

--- fzf-zsh-plugin.plugin.zsh
+++ fzf-zsh-plugin.plugin.zsh
@@ -47,7 +47,7 @@ fi
 unset xdg_path
 
 # Install fzf into ~ if it hasn't already been installed.
-if [[ ! _fzf_has fzf ]]; then
+if ! _fzf_has fzf; then
   if [[ ! -d $FZF_PATH ]]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git $FZF_PATH
     $FZF_PATH/install --bin
