d668cf9188a0852d0f87ff9c29d47423b80d2952
Joe Block <jpb@unixorn.net>
2025-10-01 21:39:54 -0600

    Fix #125, hopefully
    Signed-off-by: Joe Block <jpb@unixorn.net>

--- fzf-zsh-plugin.plugin.zsh
+++ fzf-zsh-plugin.plugin.zsh
@@ -1,4 +1,4 @@
-# Copyright 2020-2024 Joseph Block <jpb@unixorn.net>
+# Copyright 2020-2025 Joseph Block <jpb@unixorn.net>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
